b210013d22baa0f96c49cf646a99bee3c51617f9
vivekrqwat <vivekrawaturfsam@gmail.com>
2026-09-09 13:44:54 +0530

    ""[Dbcrypt added[D[D[D[D[D[D[D[D[D[D[D[[C[C[C[C[C[C[C[C[C[C[C[C[C

--- backend/src/Controller/UserController.ts
+++ backend/src/Controller/UserController.ts
@@ -2,6 +2,7 @@
 import { NextFunction, Request, Response } from "express";
 import EmailAuth from "./EmailAuth";
 import UserModel from "../Model/UserSchema";
+import bcrypt from "bcryptjs";
 const UserReg=async(req:Request,res:Response)=>{
     const{email,password}=req.body
   
@@ -10,9 +11,11 @@ const UserReg=async(req:Request,res:Response)=>{
     if (existingUser) {
       return res.status(409).json({ message: "User already registered." });
     }
+    const salt = bcrypt.genSaltSync(10);
+const hashedpassword = bcrypt.hashSync(password, salt);
         const body={
     email:email,
-    password:password
+    password:hashedpassword
 }
         const data= new UserModel(body);
         const savedData=await data.save();
